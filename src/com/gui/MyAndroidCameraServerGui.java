8ee2d3604d218f93e1a750aebb153ea5c87676e4
vicky_LinYoung <292442545@qq.com>
2019-04-29 23:57:23 +0800

    imagePartUpdate

--- src/com/gui/MyAndroidCameraServerGui.java
+++ src/com/gui/MyAndroidCameraServerGui.java
@@ -10,16 +10,29 @@ import java.awt.*;
 
 public class MyAndroidCameraServerGui {
 
+
     public static void main(String[] args) {
         //currNet
         UDPListener myUDPListener = new UDPListener(31233);
 
         //gui
         JFrame jFrame = new JFrame("Camera Monitor");
-        jFrame.setSize(800,600);
+        jFrame.setSize(1124,768);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //JPanel set component
-        jFrame.add(readyJPanel(myUDPListener));
+        JPanel the_panel = readyJPanel(myUDPListener);
+        //image
+        GridBagConstraints gbc =new GridBagConstraints();
+        gbc.gridy=2;
+        gbc.gridx=0;
+        gbc.gridwidth=GridBagConstraints.REMAINDER;
+        JLabel imLable01 = getImageLable("./res/chihaya01.jpg");
+        JLabel imLable02 = getImageLable("./res/chihaya02.jpg");
+        the_panel.add(imLable01,gbc);
+        the_panel.remove(imLable01);
+        the_panel.add(imLable02,gbc);
+        //
+        jFrame.add(the_panel);
         jFrame.setVisible(true);
     }
 
@@ -56,19 +69,25 @@ public class MyAndroidCameraServerGui {
         jTextField2.setHorizontalAlignment(JTextField.CENTER);
         panel.add(jTextField2,bagConstraints);
 
-        bagConstraints = new GridBagConstraints();
-        bagConstraints.gridy=2;
-        bagConstraints.ipady=200;
-        bagConstraints.ipadx=300;
-        bagConstraints.gridwidth=GridBagConstraints.REMAINDER;
+//        bagConstraints = new GridBagConstraints();
 //        bagConstraints.gridy=2;
-        bagConstraints.fill=GridBagConstraints.BOTH;
-        JTextField jTextField3 = new JTextField(Integer.toString(inUDPListener.getPort()));
-        jTextField3.setHorizontalAlignment(JTextField.CENTER);
-        panel.add(jTextField3,bagConstraints);
-
-
+//        bagConstraints.ipady=200;
+//        bagConstraints.ipadx=300;
+//        bagConstraints.gridwidth=GridBagConstraints.REMAINDER;
+////        bagConstraints.gridy=2;
+//        bagConstraints.fill=GridBagConstraints.BOTH;
+//        JTextField jTextField3 = new JTextField(Integer.toString(inUDPListener.getPort()));
+//        jTextField3.setHorizontalAlignment(JTextField.CENTER);
+//        panel.add(jTextField3,bagConstraints);
+//
+//
 
         return panel;
+    }
+    public   static JLabel getImageLable(String thePath){
+        JLabel imageContaniner =new JLabel();
+        ImageIcon pic = new ImageIcon(thePath);
+        imageContaniner.setIcon(pic);
+        return imageContaniner;
     }
 }
